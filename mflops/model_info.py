81fddf9407bcbdca02b9c57f6b03640b3fb94101
yu shunchang <yushunchang@dreame.tech>
2020-12-14 17:40:44 +0800

    modify author info

--- mflops/model_info.py
+++ mflops/model_info.py
@@ -1,10 +1,10 @@
-'''
-Copyright (C) 2019 Sovrasov V. - All Rights Reserved
- * You may use, distribute and modify this code under the
- * terms of the MIT license.
- * You should have received a copy of the MIT license with
- * this file. If not visit https://opensource.org/licenses/MIT
-'''
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 17:38:48 2020
+
+@author: luke
+"""
 
 import sys
 from functools import partial
